1c9e3db43eb1398645ae0de8f296470c509a351d
Pete Lelliott <petelelliott@gmail.com>
2024-11-21 12:09:56 +0000

    FINISHED: upgraded to postgres 16.6

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -161,6 +161,7 @@ data "aws_iam_policy_document" "rds" {
   statement {
     effect = "Allow"
     actions = [
+      "rds:AddTagsToResource",
       "rds:DescribeDBSubnetGroups",
       "rds:DescribeDBInstances",
       "rds:CreateDBSubnetGroup",
