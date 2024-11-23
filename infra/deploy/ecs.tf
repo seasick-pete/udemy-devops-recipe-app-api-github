77c97dcf7438bda017e6ea7ee11398080b23c8bd
Pete Lelliott <petelelliott@gmail.com>
2024-11-23 13:58:44 +0000

    FINISHED: added efs

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -94,6 +94,11 @@ resource "aws_ecs_task_definition" "api" {
             readOnly      = false
             containerPath = "/vol/web/static"
             sourceVolume  = "static"
+          },
+          {
+            readOnly      = false
+            containerPath = "/vol/web/media"
+            sourceVolume  = "efs-media"
           }
         ],
         logConfiguration = {
@@ -128,6 +133,11 @@ resource "aws_ecs_task_definition" "api" {
             readOnly      = true
             containerPath = "/vol/static"
             sourceVolume  = "static"
+          },
+          {
+            readOnly      = true
+            containerPath = "/vol/media"
+            sourceVolume  = "efs-media"
           }
         ]
         logConfiguration = {
@@ -144,6 +154,19 @@ resource "aws_ecs_task_definition" "api" {
 
   volume {
     name = "static"
+  }
+
+  volume {
+    name = "efs-media"
+    efs_volume_configuration {
+      file_system_id     = aws_efs_file_system.media.id
+      transit_encryption = "ENABLED"
+
+      authorization_config {
+        access_point_id = aws_efs_access_point.media.id
+        iam             = "DISABLED"
+      }
+    }
   }
 
   runtime_platform {
